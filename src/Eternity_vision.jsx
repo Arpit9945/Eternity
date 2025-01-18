b26ac2caed01fb72183cc846f2cf9d9d79147e49
arpitposimyth <arpit.posimyth@gmail.com>
2025-01-18 19:46:08 +0530

    set logo and create basic files

---
+++ src/Eternity_vision.jsx
@@ -0,0 +1,14 @@
+// import './style/global.scss'
+
+const Eternity_Vision = () => {
+
+  return (
+    <div className="eternity-vision">
+      <img src="/images/svg/eternity-logo.svg" />
+      <h1>eternity vision</h1>
+    </div>
+  );
+
+}
+
+export default Eternity_Vision;
